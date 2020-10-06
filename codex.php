01fef19275a826d2dcf986fadfdd04b386c431fc
roy-a2yush <roy.a2yush@gmail.com>
2020-10-06 13:55:38 +0530

    Added dahsboard, changed home link in codex

--- codex.php
+++ codex.php
@@ -93,14 +93,14 @@
 </div>
 <!-- This is navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="height:8vh;">
-  <a class="navbar-brand" href="#">codeSmode</a>
+  <a class="navbar-brand" href="dashboard.html">codeSmode</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="dashboard.html">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Questions</a>
