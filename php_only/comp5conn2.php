d8de0b44f2d8bcb3b2ce9d1ebdcd59528639766b
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2020-11-07 16:15:14 +0530

    File for executing 2 final page in admin

---
+++ php_only/comp5conn2.php
@@ -0,0 +1,10 @@
+
+<?php 
+  include "config.php";
+  session_start();
+  $qname=$_SESSION['qname'];
+  $testcase3=$_POST['testcase3'];
+  $sql="UPDATE `questions` SET `testcase3`='".$testcase3."' WHERE `qname`='".$qname."'";
+  $conn -> query($sql);  
+  header('Location: comp2.php');
+?>
