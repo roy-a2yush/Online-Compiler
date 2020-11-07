543277ac74c528b46536d24f32a2ef4a96da9e33
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2020-11-07 16:15:34 +0530

    Admin home page

---
+++ adminhome.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/compcss.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>codeSmode</title>
+</head>
+<body>
+	<nav class="navbar navbar-dark navbar-expand-sm fixed-top" style="background:black">
+    	<div class="container">
+      		<a class="navbar-brand" href="#" style="color:white"><b>codeSmode</b><br></a>
+      		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+        		<span class="navbar-toggler-icon"></span>
+      		</button>
+      		<ul style="list-style-type:none;">
+            <li type="">
+      		  	
+      		  	<a id="log" href=# class="navbar-brand pull-right" style="color:white">Logout</a>
+    		    </li>
+       	  </ul>
+        </div>
+    </nav>  
+    <br><br><br>
+    <div class="navbar" style="background-color: black" "color:white">
+    <br>
+    	<a id ="link" href="adminhome.php"><b><i>Admin Home</i></b></a><br>
+  		
+  		<a id ="link" href="comp2.php">Add Events</a><br>
+  		
+  		<a id ="link" href="edit.php">Edit</a><br>
+</div>
+	<br>
+  	
+  	<div class="card mx-auto">
+  		<div class="container">
+  			<div class="card-header text-center" style="background-color: black">
+  				<b style="color: white">Admin Panel</b>
+  			</div>
+  			<div class="card-body">	
+  				<div>Welcome to the Admin Page of CodeSmode!</div>
+          <div>Here you can <a href="comp2.php" style="color: black"><b>add</b></a> more questions to the already exisiting Questions repository or <a href="edit.php" style="color: black"><b>edit</b></a> them!</div> 
+  				
+  			</div>	
+		</div>
+	</div>	
+</body>
+</html>
